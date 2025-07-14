625e6a1a534d9e700cef556c59a925f472e75369
Misa <misa.cc2393293@gmail.com>
2025-07-14 12:01:36 -0600

    common-services

---
+++ commons/src/main/java/com/example/commons/services/CommonService.java
@@ -0,0 +1,18 @@
+package com.example.commons.services;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommonService<RQ, RS> {
+	
+	List<RS> listar();
+
+	Optional<RS> obtnerPorID(Long id);
+
+	RS insertar(RQ request);
+
+	RS actualizar(RQ request, Long id);
+
+	RS eliminar(Long id);
+	
+}
