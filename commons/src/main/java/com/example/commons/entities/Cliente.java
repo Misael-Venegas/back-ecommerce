fbcea101fb66819f4b2b6edc73f55dfd35d04978
Jaqueline027 <jaqueline53210@gmail.com>
2025-07-14 14:13:35 -0600

    Entidad cliente

--- commons/src/main/java/com/example/commons/entities/Cliente.java
+++ commons/src/main/java/com/example/commons/entities/Cliente.java
@@ -1,5 +1,94 @@
 package com.example.commons.entities;
 
-public class Cliente {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CLIENTE_SEQ")
+	@SequenceGenerator(name = "CLIENTE_SEQ", sequenceName = "CLIENTE_SEQ", allocationSize = 1)
+	@Column
+	private Long id_cliente;
+	
+	@Column(name = "NOMBRE", nullable = false)
+	private String nombre;
+	
+	@Column(name = "APELLIDO", nullable = false)
+	private String apellido;
+	
+	@Column(name = "EMAIL", nullable = false)
+	private String email;
+	
+	@Column(name = "TELEFONO", nullable = false)
+	private int telefono;
+	
+	@Column(name = "DIRECCION", nullable = false)
+	private String direccion;
+	
+	
+	public Cliente(Long id_cliente, String nombre, String apellido, String email, int telefono, String direccion) {
+		this.id_cliente = id_cliente;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public Long getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(Long id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
 }
