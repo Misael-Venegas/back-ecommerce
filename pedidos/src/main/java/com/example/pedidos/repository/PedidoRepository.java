9898edf5283a1351625cb79dae523f3ad57b42a7
Misa <misa.cc2393293@gmail.com>
2025-07-15 17:31:06 -0600

    actualizacion servicio pedidos

--- pedidos/src/main/java/com/example/pedidos/repository/PedidoRepository.java
+++ pedidos/src/main/java/com/example/pedidos/repository/PedidoRepository.java
@@ -2,15 +2,15 @@ package com.example.pedidos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.commons.entities.Pedidos;
 
+import feign.Param;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedidos, Long>{
 	
-	@Query("SELECT COUNT(p) > 0 FROM Pedido p WHERE p.idProducto = :idProducto")
+	@Query("SELECT COUNT(p) > 0 FROM Pedidos p JOIN p.productos pr WHERE pr.idProducto = :idProducto")
 	boolean existeProductoId(@Param("idProducto") Long idProducto);
-	
 }
