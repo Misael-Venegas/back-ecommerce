d4d9487e77a81dbb6de69f850db492911650e20d
Carlos <cmol110400@gmail.com>
2025-07-16 19:40:57 -0600

    Actualizacion Cors

---
+++ gateway/src/main/java/com/misa/gateway/auth/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.misa.gateway.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+	@Bean
+	CorsWebFilter corsWebFilter() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost:4200");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsWebFilter(source);
+	}
+	
+}
